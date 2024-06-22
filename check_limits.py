5adc8ddd3106276a66216f0fcd54cf18164b3d92
Ananyaa-holla <49331515+Ananyaa-holla@users.noreply.github.com>
2024-06-22 09:14:32 +0530

    Reducing the cyclomatic complexity

--- check_limits.py
+++ check_limits.py
@@ -1,16 +1,8 @@
-
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
-
-  return True
+  if (temperature < 0 or temperature > 45) or (soc < 20 or soc > 80) or (charge_rate > 0.8):
+      return False
+  else:
+    return True
 
 
 if __name__ == '__main__':
